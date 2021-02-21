7a0326b746df5f5425786f88bb46bc7610be6199
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2021-02-21 18:14:08 +0100

    Update copyright statements

--- fieldfilterproxymodel.cpp
+++ fieldfilterproxymodel.cpp
@@ -1,3 +1,8 @@
+/*
+    SPDX-License-Identifier: LGPL-2.1-or-later OR MIT
+    SPDX-FileCopyrightText: 2021 Andreas Cord-Landwehr <cordlandwehr@kde.org>
+*/
+
 #include "fieldfilterproxymodel.h"
 #include "journaldviewmodel.h"
 #include "loggingcategories.h"
--- fieldfilterproxymodel.h
+++ fieldfilterproxymodel.h
@@ -1,3 +1,8 @@
+/*
+    SPDX-License-Identifier: LGPL-2.1-or-later OR MIT
+    SPDX-FileCopyrightText: 2021 Andreas Cord-Landwehr <cordlandwehr@kde.org>
+*/
+
 #ifndef FIELDFILTERPROXYMODEL_H
 #define FIELDFILTERPROXYMODEL_H
 
