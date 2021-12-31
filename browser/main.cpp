9ced65fed35e04bb05acbb5c9b78830b1680c09d
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2021-12-31 10:34:25 +0100

    Remove obsolete models
    These were replaced by filtercriteriamodel

--- browser/main.cpp
+++ browser/main.cpp
@@ -73,67 +73,31 @@ int main(int argc, char *argv[])
     SessionConfig sessionConfig;
     BootModel bootModel;
 
-    JournaldUniqueQueryModel unitModel;
-    unitModel.setField(JournaldHelper::Field::SYSTEMD_UNIT);
-    QSortFilterProxyModel unitSortProxyModel;
-    unitSortProxyModel.setSourceModel(&unitModel);
-    unitSortProxyModel.setSortCaseSensitivity(Qt::CaseInsensitive);
-    unitSortProxyModel.setSortRole(JournaldUniqueQueryModel::FIELD);
-    QObject::connect(&unitModel, &JournaldUniqueQueryModel::modelReset, [&unitSortProxyModel]() {
-        unitSortProxyModel.sort(0);
-    });
-    unitSortProxyModel.sort(0);
-
-    JournaldUniqueQueryModel executableModel;
-    executableModel.setField(JournaldHelper::Field::EXE);
-    QSortFilterProxyModel executableSortProxyModel;
-    executableSortProxyModel.setSourceModel(&executableModel);
-    executableSortProxyModel.setSortCaseSensitivity(Qt::CaseInsensitive);
-    executableSortProxyModel.setSortRole(JournaldUniqueQueryModel::FIELD);
-    QObject::connect(&executableModel, &JournaldUniqueQueryModel::modelReset, [&executableSortProxyModel]() {
-        executableSortProxyModel.sort(0);
-    });
-    executableSortProxyModel.sort(0);
-
     FilterCriteriaModel filterCriteriaModel;
 
-    QObject::connect(&sessionConfig,
-                     &SessionConfig::modeChanged,
-                     &sessionConfig,
-                     [&sessionConfig, &bootModel, &unitModel, &executableModel, &filterCriteriaModel](SessionConfig::Mode mode) {
-                         switch (mode) {
-                         case SessionConfig::Mode::SYSTEM:
-                             bootModel.setSystemJournal();
-                             unitModel.setSystemJournal();
-                             executableModel.setSystemJournal();
-                             filterCriteriaModel.setSystemJournal();
-                             break;
-                         case SessionConfig::Mode::REMOTE:
-                             // remote is handle like a local access
-                             Q_FALLTHROUGH();
-                         case SessionConfig::Mode::LOCALFOLDER:
-                             bootModel.setJournaldPath(sessionConfig.localJournalPath());
-                             unitModel.setJournaldPath(sessionConfig.localJournalPath());
-                             executableModel.setJournaldPath(sessionConfig.localJournalPath());
-                             filterCriteriaModel.setJournaldPath(sessionConfig.localJournalPath());
-                             break;
-                         }
-                     });
-    QObject::connect(&sessionConfig,
-                     &SessionConfig::localJournalPathChanged,
-                     &sessionConfig,
-                     [&sessionConfig, &bootModel, &unitModel, &executableModel, &filterCriteriaModel]() {
-                         bootModel.setJournaldPath(sessionConfig.localJournalPath());
-                         unitModel.setJournaldPath(sessionConfig.localJournalPath());
-                         executableModel.setJournaldPath(sessionConfig.localJournalPath());
-                         filterCriteriaModel.setJournaldPath(sessionConfig.localJournalPath());
-                     });
+    QObject::connect(&sessionConfig, &SessionConfig::modeChanged, &sessionConfig, [&sessionConfig, &bootModel, &filterCriteriaModel](SessionConfig::Mode mode) {
+        switch (mode) {
+        case SessionConfig::Mode::SYSTEM:
+            bootModel.setSystemJournal();
+            filterCriteriaModel.setSystemJournal();
+            break;
+        case SessionConfig::Mode::REMOTE:
+            // remote is handle like a local access
+            Q_FALLTHROUGH();
+        case SessionConfig::Mode::LOCALFOLDER:
+            bootModel.setJournaldPath(sessionConfig.localJournalPath());
+            filterCriteriaModel.setJournaldPath(sessionConfig.localJournalPath());
+            break;
+        }
+    });
+    QObject::connect(&sessionConfig, &SessionConfig::localJournalPathChanged, &sessionConfig, [&sessionConfig, &bootModel, &filterCriteriaModel]() {
+        bootModel.setJournaldPath(sessionConfig.localJournalPath());
+        filterCriteriaModel.setJournaldPath(sessionConfig.localJournalPath());
+    });
 
     if (parser.isSet(pathOption)) {
         const QString path = parser.value(pathOption);
         bootModel.setJournaldPath(path);
-        unitModel.setJournaldPath(path);
-        executableModel.setJournaldPath(path);
         sessionConfig.setLocalJournalPath(path);
         sessionConfig.setMode(SessionConfig::Mode::LOCALFOLDER);
     }
@@ -151,10 +115,6 @@ int main(int argc, char *argv[])
         },
         Qt::QueuedConnection);
     engine.rootContext()->setContextProperty("g_bootModel", &bootModel);
-    engine.rootContext()->setContextProperty("g_unitModel", &unitModel);
-    engine.rootContext()->setContextProperty("g_unitSortProxyModel", &unitSortProxyModel);
-    engine.rootContext()->setContextProperty("g_executableModel", &executableModel);
-    engine.rootContext()->setContextProperty("g_executableSortProxyModel", &executableSortProxyModel);
     engine.rootContext()->setContextProperty("g_config", &sessionConfig);
     engine.rootContext()->setContextProperty("g_filterModel", &filterCriteriaModel);
 
