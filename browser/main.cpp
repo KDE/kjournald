a834309d2ab6ed5e22057c901203ace8e7d29e03
Laurent Montel <montel@kde.org>
2022-08-15 16:54:24 +0200

    Remove warning when we build against qt6

--- browser/main.cpp
+++ browser/main.cpp
@@ -29,7 +29,9 @@
 
 int main(int argc, char *argv[])
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
     QApplication app(argc, argv);
     QGuiApplication::setWindowIcon(QIcon::fromTheme(QStringLiteral("utilities-log-viewer")));
     app.setOrganizationName("KDE");
